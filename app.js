d62ee5dd8aaf2c08d0631db80654ad036aaba753
Yogi <137255136+BEEYOGS@users.noreply.github.com>
2024-01-11 17:01:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('/sw.js')
     .then(function(registration) {
       console.log('Service Worker registered with scope:', registration.scope);
     })
